25d1e3cfb878004e162f0c2976ceafe5e2ff4f91
tsiort <tsiotrasi@gmail.com>
2018-10-26 16:56:25 +0300

    redisign

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -4,7 +4,6 @@ var register = function(Handlebars) {
   var helpers = {
     // put all of your helpers inside this object
     ifCond: function(v1, operator, v2, options) {
-      console.log(v1 + ' ' + operator + ' ' + v2);
       switch (operator) {
         case '==':
           return (v1 == v2) ? options.fn(this) : options.inverse(this);
@@ -39,6 +38,31 @@ var register = function(Handlebars) {
     dateFormat: function(date) {
       return moment(date).format('DD-MM-YYYY');
     },
+    getLayout: function(length, index) {
+      // Make sure last line is always field
+      let last_line_items_no = length % 3;
+      let def_col_grid = '4';
+      let col_grid;
+
+      if (index == length - 1) {
+
+        switch (last_line_items_no) {
+          case 0:
+            return def_col_grid;
+          case 1:
+            col_grid = '12';
+            return col_grid;
+          case 2:
+            col_grid = '8';
+            return col_grid;
+          default:
+            return def_col_grid;
+        }
+      }
+      else return def_col_grid;
+    },
+
+
 
   };
 
